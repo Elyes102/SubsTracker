dbb85e15338252628c5043e7362dcd57a4080b1e
Elyes102 <isme2473@Usherbrooke.ca>
2025-06-20 14:42:44 -0400

    INIT

---
+++ src/test/java/com/example/substracker/SubstrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.substracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubstrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
